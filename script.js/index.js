7ca717b51fc8049d5afd8102d11fda401eeac417
Anastasya Yakimec <youremail@gmail.com>
2022-06-03 20:03:51 +0300

    add ex_4-swith

---
+++ script.js/index.js
@@ -0,0 +1,17 @@
+let user = "superAdmin";
+switch (user) {
+    case "user" :
+        alert("View,share,copy");
+        break;   
+    case "admin":
+        alert("Security settings,software");
+        break; 
+      case "moderator":
+        alert("Delete message,editing message");
+        break; 
+      case "superAdmin":
+        alert("Adding an administrator");
+        break; 
+
+        
+}
